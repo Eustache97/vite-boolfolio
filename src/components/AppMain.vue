a4f4b6d6c77a905179930e796ab2a2a9304a8e82
Eustache97 <eubomber97@gmail.com>
2023-01-22 18:34:06 +0100

    add ProjectCard

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,9 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 export default {
     name: "AppMain",
+    components: { ProjectCard },
     data() {
         return {
             descriptionMaxLength: 150,
@@ -16,34 +18,18 @@ export default {
         getProjects() {
             axios.get(`${this.urlPrefix}/api/projects`).then(resp => {
                 this.projects = resp.data.results;
+                console.log(this.projects);
             });
-        },
-        descriptionPreview(text) {
-            if (text && text.length > this.descriptionMaxLength) {
-                return text.substr(0, this.descriptionMaxLength) + '...';
-            }
-            return text;
-    }
+        }
     }
 }
 </script>
 
 <template>
     <div class="container">
-        <h2 class="text-center mb-3">Tutti i progetti</h2>
+        <h1 class="text-center mb-5">Tutti i progetti</h1>
         <div class="row row-cols-3 justify-content-between gy-5">
-            <div class="card" 
-            v-for="project in projects" key="project.id" style="width: 18rem;">    
-                <img v-if="project.cover_image" :src="`${urlPrefix}/storage/${project.cover_image}`" class="card-img-top" alt="">
-                <div v-else class="w-100 bg-secondary text-white" style="height:150px">
-                        Nessuna immagine
-                    </div>
-                <div class="card-body">
-                    <h5 class="card-title">{{ project.title }}</h5>
-                    <p class="card-text">{{ descriptionPreview(project.description) }}</p>
-                    <a href="#" class="btn btn-primary">Vedi</a>
-                </div>
-            </div>
+            <ProjectCard :project="project" v-for="project in projects" :key="project.id"/>
         </div>  
     </div>
 </template>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,40 @@
+<script>
+    import { computed } from '@vue/reactivity';
+    export default{
+        name: 'ProjectCard',
+        data() {
+            return{
+                urlPrefix: "http://127.0.0.1:8000",
+                descriptionMaxLength: 150
+            }
+        },
+        props: {
+            project: Object,
+        },
+        computed: {
+            descriptionPreview() {
+                if (this.project.description && this.project.description.length > this.descriptionMaxLength) {
+                    return this.project.description.substr(0, this.descriptionMaxLength) + '...';
+                }
+                    return this.project.description;
+            }
+        },
+    }
+</script>
+
+<template>
+    <div class="col card" style="width: 18rem;">    
+        <img v-if="project.cover_image" :src="`${urlPrefix}/storage/${project.cover_image}`" class="card-img-top" alt="">
+        <div v-else class="w-100 bg-secondary text-white" style="height:150px">
+             Nessuna immagine
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text text-secondary">{{ descriptionPreview }}</p>
+        </div>
+        <a href="#" class="btn btn-primary">Vedi</a>
+    </div>
+</template>
+
+<style lang="scss">
+</style>
